ebe4ba4d3a3de39b728be1ba3843c66996819e7d
Ryan de Oliveira Sousa <ryansousa.9810@gmail.com>
2021-02-08 14:39:56 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ Teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Olá Mundo");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
